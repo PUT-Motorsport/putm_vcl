3e9c87e834fd1ee8b00c355a37968aeca9af1b9d
kazuhiroo <antoni.katowski@student.put.poznan.pl>
2023-12-12 21:39:56 +0100

    including .hpp for rear wheels

--- amk/include/can_library/can_common.hpp
+++ amk/include/can_library/can_common.hpp
@@ -3,6 +3,9 @@
 #include "can_headers/PM08-CANBUS-AMK-FRONT-LEFT.hpp"
 #include "can_headers/PM08-CANBUS-AMK-FRONT-RIGHT.hpp"
 
+#include "can_headers/PM08-CANBUS-AMK-REAR-LEFT.hpp"
+#include "can_headers/PM08-CANBUS-AMK-REAR-RIGHT.hpp"
+
 namespace PUTM_CAN {
 static constexpr int INVALID_FILE_DESCRIPTOR = -1;
 static constexpr uint16_t INVALID_CAN_ID = 0xFFFFU;
@@ -23,4 +26,20 @@ template <>
 constexpr uint16_t can_id<AmkFrontRightActualValues2> = FRONT_RIGHT_AMK_ACTUAL_VALUES_2_CAN_ID;
 template <>
 constexpr uint16_t can_id<AmkFrontRightSetpoints1> = FRONT_RIGHT_AMK_SETPOINTS_1_CAN_ID;
+
+/*
+template <>
+constexpr uint16_t can_id<AmkRearLeftActualValues1> = REAR_LEFT_AMK_VALUES_1_CAN_ID;
+template <>
+constexpr uint16_t can_id<AmkRearLeftActualValues2> = REAR_LEFT_AMK_ACTUAL_VALUES_2_CAN_ID;
+template <>
+constexpr uint16_t can_id<AmkRearLeftSetpoints1> = REAR_LEFT_AMK_SETPOINTS_1_CAN_ID;
+
+template <>
+constexpr uint16_t can_id<AmkRearRightActualValues1> = REAR_RIGHT_AMK_VALUES_1_CAN_ID;
+template <>
+constexpr uint16_t can_id<AmkRearRightActualValues2> = REAR_RIGHT_AMK_ACTUAL_VALUES_2_CAN_ID;
+template <>
+constexpr uint16_t can_id<AmkRearRightSetpoints1> = REAR_RIGHT_AMK_SETPOINTS_1_CAN_ID;
+*/
 }  // namespace PUTM_CAN
