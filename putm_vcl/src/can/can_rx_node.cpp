2bf002cba9f2aa59c71f4ccb21f084d7f83b48f8
KozAAAAA <mateusz-koza@outlook.com>
2024-05-28 12:19:55 +0200

    Change defines to can_id templates, change names

--- putm_vcl/src/can/can_rx_node.cpp
+++ putm_vcl/src/can/can_rx_node.cpp
@@ -1,9 +1,9 @@
 #include "PUTM_DV_CAN_LIBRARY_RAII_2024/include/can_headers/PM09-CANBUS-FRONTBOX.hpp"
 #include "PUTM_DV_CAN_LIBRARY_RAII_2024/include/can_rx.hpp"
+#include "putm_vcl_interfaces/msg/amk_data.hpp"
+#include "putm_vcl_interfaces/msg/amk_status.hpp"
 #include "putm_vcl_interfaces/msg/detail/frontbox__struct.hpp"
 #include "putm_vcl_interfaces/msg/frontbox.hpp"
-#include "putm_vcl_interfaces/msg/amk_status.hpp"
-#include "putm_vcl_interfaces/msg/amk_data.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/utilities.hpp"
 
@@ -60,56 +60,61 @@ void CanRxNode::can_rx_common_callback() {
 void CanRxNode::can_rx_amk_callback() {
   can_frame frame = can_rx_amk.receive();
   switch (frame.can_id) {
-    case FRONT_LEFT_AMK_ACTUAL_VALUES_1_CAN_ID: {
-      auto frontLeftAmkActualValues1 = convert<AmkFrontLeftActualValues1>(frame);
-      amk_status.amk_status_bsystem_ready[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bSystemReady;
-      amk_status.amk_status_berror[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bError;
-      amk_status.amk_status_bwarn[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bWarn;
-      amk_status.amk_status_bquit_dc_on[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bQuitDcOn;
-      amk_status.amk_status_bdc_on[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bDcOn;
-      amk_status.amk_status_bquit_inverter_on[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bQuitInverterOn;
-      amk_status.amk_status_binverter_on[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bInverterOn;
-      amk_status.amk_status_bderating[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_Status.AMK_bDerating;
+    case can_id<AmkFrontLeftActualValues1>: {
+      auto can_amk = convert<AmkFrontLeftActualValues1>(frame);
+      amk_status.amk_status_bsystem_ready[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bSystemReady;
+      amk_status.amk_status_berror[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bError;
+      amk_status.amk_status_bwarn[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bWarn;
+      amk_status.amk_status_bquit_dc_on[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bQuitDcOn;
+      amk_status.amk_status_bdc_on[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bDcOn;
+      amk_status.amk_status_bquit_inverter_on[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bQuitInverterOn;
+      amk_status.amk_status_binverter_on[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bInverterOn;
+      amk_status.amk_status_bderating[Inverters::FRONT_LEFT] = can_amk.AMK_Status.AMK_bDerating;
 
-      amk_data.amk_actual_velocity[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_ActualVelocity;
-      amk_data.amk_torque_current[Inverters::FRONT_LEFT] = frontLeftAmkActualValues1.AMK_TorqueCurrent;
-    } break;
+      // TODO: Determine why only there
+      amk_data.amk_actual_velocity[Inverters::FRONT_LEFT] = can_amk.AMK_ActualVelocity;
+      amk_data.amk_torque_current[Inverters::FRONT_LEFT] = can_amk.AMK_TorqueCurrent;
+      break;
+    }
 
-    case FRONT_RIGHT_AMK_ACTUAL_VALUES_1_CAN_ID: {
-      auto frontRightAmkActualValues1 = convert<AmkFrontRightActualValues1>(frame);
-      amk_status.amk_status_bsystem_ready[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bSystemReady;
-      amk_status.amk_status_berror[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bError;
-      amk_status.amk_status_bwarn[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bWarn;
-      amk_status.amk_status_bquit_dc_on[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bQuitDcOn;
-      amk_status.amk_status_bdc_on[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bDcOn;
-      amk_status.amk_status_bquit_inverter_on[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bQuitInverterOn;
-      amk_status.amk_status_binverter_on[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bInverterOn;
-      amk_status.amk_status_bderating[Inverters::FRONT_RIGHT] = frontRightAmkActualValues1.AMK_Status.AMK_bDerating;
-    } break;
+    case can_id<AmkFrontRightActualValues1>: {
+      auto can_amk = convert<AmkFrontRightActualValues1>(frame);
+      amk_status.amk_status_bsystem_ready[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bSystemReady;
+      amk_status.amk_status_berror[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bError;
+      amk_status.amk_status_bwarn[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bWarn;
+      amk_status.amk_status_bquit_dc_on[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bQuitDcOn;
+      amk_status.amk_status_bdc_on[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bDcOn;
+      amk_status.amk_status_bquit_inverter_on[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bQuitInverterOn;
+      amk_status.amk_status_binverter_on[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bInverterOn;
+      amk_status.amk_status_bderating[Inverters::FRONT_RIGHT] = can_amk.AMK_Status.AMK_bDerating;
+      break;
+    }
 
-    case REAR_LEFT_AMK_ACTUAL_VALUES_1_CAN_ID: {
-      auto rearLeftAmkActualValues1 = convert<AmkRearLeftActualValues1>(frame);
-      amk_status.amk_status_bsystem_ready[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bSystemReady;
-      amk_status.amk_status_berror[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bError;
-      amk_status.amk_status_bwarn[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bWarn;
-      amk_status.amk_status_bquit_dc_on[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bQuitDcOn;
-      amk_status.amk_status_bdc_on[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bDcOn;
-      amk_status.amk_status_bquit_inverter_on[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bQuitInverterOn;
-      amk_status.amk_status_binverter_on[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bInverterOn;
-      amk_status.amk_status_bderating[Inverters::REAR_LEFT] = rearLeftAmkActualValues1.AMK_Status.AMK_bDerating;
-    } break;
+    case can_id<AmkRearLeftActualValues1>: {
+      auto can_amk = convert<AmkRearLeftActualValues1>(frame);
+      amk_status.amk_status_bsystem_ready[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bSystemReady;
+      amk_status.amk_status_berror[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bError;
+      amk_status.amk_status_bwarn[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bWarn;
+      amk_status.amk_status_bquit_dc_on[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bQuitDcOn;
+      amk_status.amk_status_bdc_on[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bDcOn;
+      amk_status.amk_status_bquit_inverter_on[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bQuitInverterOn;
+      amk_status.amk_status_binverter_on[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bInverterOn;
+      amk_status.amk_status_bderating[Inverters::REAR_LEFT] = can_amk.AMK_Status.AMK_bDerating;
+      break;
+    }
 
-    case REAR_RIGHT_AMK_ACTUAL_VALUES_1_CAN_ID: {
-      auto rearRightAmkActualValues1 = convert<AmkRearRightActualValues1>(frame);
-      amk_status.amk_status_bsystem_ready[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bSystemReady;
-      amk_status.amk_status_berror[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bError;
-      amk_status.amk_status_bwarn[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bWarn;
-      amk_status.amk_status_bquit_dc_on[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bQuitDcOn;
-      amk_status.amk_status_bdc_on[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bDcOn;
-      amk_status.amk_status_bquit_inverter_on[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bQuitInverterOn;
-      amk_status.amk_status_binverter_on[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bInverterOn;
-      amk_status.amk_status_bderating[Inverters::REAR_RIGHT] = rearRightAmkActualValues1.AMK_Status.AMK_bDerating;
-    } break;
+    case can_id<AmkRearRightActualValues1>: {
+      auto can_amk = convert<AmkRearRightActualValues1>(frame);
+      amk_status.amk_status_bsystem_ready[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bSystemReady;
+      amk_status.amk_status_berror[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bError;
+      amk_status.amk_status_bwarn[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bWarn;
+      amk_status.amk_status_bquit_dc_on[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bQuitDcOn;
+      amk_status.amk_status_bdc_on[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bDcOn;
+      amk_status.amk_status_bquit_inverter_on[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bQuitInverterOn;
+      amk_status.amk_status_binverter_on[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bInverterOn;
+      amk_status.amk_status_bderating[Inverters::REAR_RIGHT] = can_amk.AMK_Status.AMK_bDerating;
+      break;
+    }
   }
   amk_status_publisher->publish(amk_status);
   amk_data_publisher->publish(amk_data);
