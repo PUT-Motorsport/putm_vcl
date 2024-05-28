e8067f051ee6600ea0300d41dff727a0eef51c33
KozAAAAA <mateusz-koza@outlook.com>
2024-05-28 14:19:09 +0200

    Merge amk_tx and can_tx into one

--- putm_vcl/launch/putm_vcl.launch.py
+++ putm_vcl/launch/putm_vcl.launch.py
@@ -1,37 +1,24 @@
 import launch
 import launch_ros.actions
 
+
 def generate_launch_description():
-    return launch.LaunchDescription([
-        # amk bridge
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='amk_rx_bridge',
-            name='amk_rx_bridge'),
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='amk_tx_bridge',
-            name='amk_tx_bridge'),
-        
-        # amk node
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='amk_node',
-            name='amk_node'),
-
-        # can bridge
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='can_rx_bridge',
-            name='can_rx_bridge'),
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='can_tx_bridge',
-            name='can_tx_bridge'),
-
-        # rtd node
-        launch_ros.actions.Node(
-            package='putm_vcl',
-            executable='rtd_node',
-            name='rtd_node'),
-  ])
+    return launch.LaunchDescription(
+        [
+            # can nodes
+            launch_ros.actions.Node(
+                package="putm_vcl", executable="can_rx_node", name="can_rx_node"
+            ),
+            launch_ros.actions.Node(
+                package="putm_vcl", executable="can_tx_node", name="can_tx_node"
+            ),
+            # amk node
+            launch_ros.actions.Node(
+                package="putm_vcl", executable="amk_node", name="amk_node"
+            ),
+            # rtd node
+            launch_ros.actions.Node(
+                package="putm_vcl", executable="rtd_node", name="rtd_node"
+            ),
+        ]
+    )
---
+++ putm_vcl/src/can_nodes/can_tx_node.cpp
@@ -0,0 +1,104 @@
+#include "PUTM_DV_CAN_LIBRARY_RAII_2024/include/can_tx.hpp"
+#include "putm_vcl_interfaces/msg/amk_control.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+using namespace PUTM_CAN;
+using namespace putm_vcl_interfaces;
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class CanTxNode : public rclcpp::Node {
+ public:
+  CanTxNode();
+
+ private:
+  enum Inverters { FRONT_LEFT, FRONT_RIGHT, REAR_LEFT, REAR_RIGHT };  // TODO: Move to a common .hpp file
+
+  CanTx can_tx_amk;
+  CanTx can_tx_common;
+
+  AmkFrontLeftSetpoints1 amk_front_left_setpoints;
+  AmkFrontRightSetpoints1 amk_front_right_setpoints;
+  AmkRearLeftSetpoints1 amk_rear_left_setpoints;
+  AmkRearRightSetpoints1 amk_rear_right_setpoints;
+
+  rclcpp::TimerBase::SharedPtr can_tx_amk_timer;
+  rclcpp::TimerBase::SharedPtr can_tx_common_timer;
+
+  rclcpp::Subscription<msg::AmkControl>::SharedPtr amk_control_subscription;
+
+  void can_tx_amk_callback();
+  void can_tx_common_callback();
+
+  void amk_control_callback(const msg::AmkControl msg);
+};
+
+CanTxNode::CanTxNode()
+    : Node("can_tx_node"),
+      can_tx_amk("can0"),  // TODO: Move to a common .hpp file
+      can_tx_common("can1"),
+      can_tx_amk_timer(this->create_wall_timer(10ms, std::bind(&CanTxNode::can_tx_amk_callback, this))),
+      can_tx_common_timer(this->create_wall_timer(10ms, std::bind(&CanTxNode::can_tx_common_callback, this))),
+      amk_control_subscription(this->create_subscription<msg::AmkControl>("putm_vcl/amk_control", 1, std::bind(&CanTxNode::amk_control_callback, this, _1))) {}
+
+void CanTxNode::amk_control_callback(const msg::AmkControl msg) {
+  
+  amk_front_left_setpoints.AMK_TargetVelocity = msg.amk_target_torque[Inverters::FRONT_LEFT];
+  amk_front_left_setpoints.AMK_TorqueLimitPositiv = msg.amk_torque_positive_limit[Inverters::FRONT_LEFT];
+  amk_front_left_setpoints.AMK_TorqueLimitNegativ = msg.amk_torque_negative_limit[Inverters::FRONT_LEFT];
+  amk_front_left_setpoints.AMK_Control = {0,
+                                          msg.amk_control_binverter_on[Inverters::FRONT_LEFT],
+                                          msg.amk_control_bdc_on[Inverters::FRONT_LEFT],
+                                          msg.amk_control_benable[Inverters::FRONT_LEFT],
+                                          msg.amk_control_amkb_error_reset[Inverters::FRONT_LEFT],
+                                          0};
+
+  amk_front_right_setpoints.AMK_TargetVelocity = msg.amk_target_torque[Inverters::FRONT_RIGHT];
+  amk_front_right_setpoints.AMK_TorqueLimitPositiv = msg.amk_torque_positive_limit[Inverters::FRONT_RIGHT];
+  amk_front_right_setpoints.AMK_TorqueLimitNegativ = msg.amk_torque_negative_limit[Inverters::FRONT_RIGHT];
+  amk_front_right_setpoints.AMK_Control = {0,
+                                           msg.amk_control_binverter_on[Inverters::FRONT_RIGHT],
+                                           msg.amk_control_bdc_on[Inverters::FRONT_RIGHT],
+                                           msg.amk_control_benable[Inverters::FRONT_RIGHT],
+                                           msg.amk_control_amkb_error_reset[Inverters::FRONT_RIGHT],
+                                           0};
+
+  amk_rear_left_setpoints.AMK_TargetVelocity = msg.amk_target_torque[Inverters::REAR_LEFT];
+  amk_rear_left_setpoints.AMK_TorqueLimitPositiv = msg.amk_torque_positive_limit[Inverters::REAR_LEFT];
+  amk_rear_left_setpoints.AMK_TorqueLimitNegativ = msg.amk_torque_negative_limit[Inverters::REAR_LEFT];
+  amk_rear_left_setpoints.AMK_Control = {0,
+                                         msg.amk_control_binverter_on[Inverters::REAR_LEFT],
+                                         msg.amk_control_bdc_on[Inverters::REAR_LEFT],
+                                         msg.amk_control_benable[Inverters::REAR_LEFT],
+                                         msg.amk_control_amkb_error_reset[Inverters::REAR_LEFT],
+                                         0};
+
+  amk_rear_right_setpoints.AMK_TargetVelocity = msg.amk_target_torque[Inverters::REAR_RIGHT];
+  amk_rear_right_setpoints.AMK_TorqueLimitPositiv = msg.amk_torque_positive_limit[Inverters::REAR_RIGHT];
+  amk_rear_right_setpoints.AMK_TorqueLimitNegativ = msg.amk_torque_negative_limit[Inverters::REAR_RIGHT];
+  amk_rear_right_setpoints.AMK_Control = {0,
+                                          msg.amk_control_binverter_on[Inverters::REAR_RIGHT],
+                                          msg.amk_control_bdc_on[Inverters::REAR_RIGHT],
+                                          msg.amk_control_benable[Inverters::REAR_RIGHT],
+                                          msg.amk_control_amkb_error_reset[Inverters::REAR_RIGHT],
+                                          0};
+}
+
+void CanTxNode::can_tx_amk_callback() {
+  can_tx_amk.transmit(amk_front_left_setpoints);
+  can_tx_amk.transmit(amk_front_right_setpoints);
+  can_tx_amk.transmit(amk_rear_left_setpoints);
+  can_tx_amk.transmit(amk_rear_right_setpoints);
+}
+
+void CanTxNode::can_tx_common_callback() {
+  // Add common tx in here
+  // Example:
+  // can_tx_common.transmit(frontbox);
+}
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CanTxNode>());
+  rclcpp::shutdown();
+}
