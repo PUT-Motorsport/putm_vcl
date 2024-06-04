2fca16299ae50eb62c075b6002fab4e1fa1360c8
KozAAAAA <mateusz-koza@outlook.com>
2024-06-04 21:47:53 +0200

    Disable rtd

--- putm_vcl/src/rtd_node/rtd_node.cpp
+++ putm_vcl/src/rtd_node/rtd_node.cpp
@@ -8,28 +8,24 @@ RtdNode::RtdNode()
     : Node("rtd_node"),
       rtd_timer(this->create_wall_timer(100ms, std::bind(&RtdNode::rtd_callback, this))),
       rtd_publisher(this->create_publisher<msg::Rtd>("putm_vcl/rtd", 1)),
-      frontbox_driver_input_subscriber(this->create_subscription<msg::FrontboxDriverInput>("putm_vcl/frontbox_driver_input", 1, std::bind(&RtdNode::frontbox_driver_input_callback, this, _1))),
+      frontbox_driver_input_subscriber(
+          this->create_subscription<msg::FrontboxDriverInput>("putm_vcl/frontbox_driver_input", 1, std::bind(&RtdNode::frontbox_driver_input_callback, this, _1))),
       dashboard_subscriber(this->create_subscription<msg::Dashboard>("putm_vcl/dashboard", 1, std::bind(&RtdNode::dashboard_callback, this, _1))) {}
 
 void RtdNode::rtd_callback() {
   /* Entry condition */
   if ((frontbox_driver_input.brake_pressure_front >= 2200.0) or (frontbox_driver_input.brake_pressure_rear >= 2200.0)) {
-    RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000,  "Braking...");
+    RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "Braking...");
     if ((dashboard.rtd_button) and not(rtd.state)) {
-      RCLCPP_INFO(this->get_logger(), "RTD ON");
+      RCLCPP_INFO(this->get_logger(), "RTD: on");
       rtd.state = true;
       rtd_publisher->publish(rtd);
     }
-  } else if (rtd.state & dashboard.rtd_button) {
-    RCLCPP_INFO(this->get_logger(), "RTD OFF");
+  } else if (rtd.state and dashboard.rtd_button) {
+    RCLCPP_INFO(this->get_logger(), "RTD: off");
+    rtd.state = false;
     rtd_publisher->publish(rtd);
   }
-  /* Exit conditions */
-  // if ((rtd_state) and (rtd_button))
-  // {
-  //   rtd_state = false;
-  //   RCLCPP_INFO(this->get_logger(), "RTD OFF");
-  // }
 }
 
 void RtdNode::frontbox_driver_input_callback(const msg::FrontboxDriverInput::SharedPtr msg) { frontbox_driver_input = *msg; }
