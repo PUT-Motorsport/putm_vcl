175ff92f418710ec5d659fc618bc92da83a37a60
KozAAAAA <mateusz-koza@outlook.com>
2024-06-04 21:32:20 +0200

    Change brake pressure and logging type

--- putm_vcl/src/rtd_node/rtd_node.cpp
+++ putm_vcl/src/rtd_node/rtd_node.cpp
@@ -13,8 +13,8 @@ RtdNode::RtdNode()
 
 void RtdNode::rtd_callback() {
   /* Entry condition */
-  if ((frontbox_driver_input.brake_pressure_front >= 100.0) or (frontbox_driver_input.brake_pressure_rear >= 100.0)) {
-    RCLCPP_INFO_ONCE(this->get_logger(), "Braking...");
+  if ((frontbox_driver_input.brake_pressure_front >= 2200.0) or (frontbox_driver_input.brake_pressure_rear >= 2200.0)) {
+    RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000,  "Braking...");
     if ((dashboard.rtd_button) and not(rtd.state)) {
       RCLCPP_INFO(this->get_logger(), "RTD ON");
       rtd.state = true;
