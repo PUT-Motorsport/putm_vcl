bc61256944d72d3a514407bc112151837382ed9b
Adam <Adam_Wasilewski_99@proton.me>
2024-10-05 23:31:21 +0200

    Fix xsens msg

--- putm_vcl/launch/putm_vcl.launch.py
+++ putm_vcl/launch/putm_vcl.launch.py
@@ -11,23 +11,23 @@ def generate_launch_description():
                 executable="can_rx_node",
                 name="can_rx_node",
             ),
-            launch_ros.actions.Node(
-                namespace="putm_vcl",
-                package="putm_vcl",
-                executable="can_tx_node",
-                name="can_tx_node",
-            ),
-            launch_ros.actions.Node(
-                namespace="putm_vcl",
-                package="putm_vcl",
-                executable="amk_node",
-                name="amk_node",
-            ),
-            launch_ros.actions.Node(
-                namespace="putm_vcl",
-                package="putm_vcl",
-                executable="rtd_node",
-                name="rtd_node",
-            ),
+            # launch_ros.actions.Node(
+            #     namespace="putm_vcl",
+            #     package="putm_vcl",
+            #     executable="can_tx_node",
+            #     name="can_tx_node",
+            # ),
+            # launch_ros.actions.Node(
+            #     namespace="putm_vcl",
+            #     package="putm_vcl",
+            #     executable="amk_node",
+            #     name="amk_node",
+            # ),
+            # launch_ros.actions.Node(
+            #     namespace="putm_vcl",
+            #     package="putm_vcl",
+            #     executable="rtd_node",
+            #     name="rtd_node",
+            # ),
         ]
     )
